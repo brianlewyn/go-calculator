97a2d08af5d2de77694bbf850bba17b92a490d1d
brianlewyn <brianlewyn@gmail.com>
2023-02-01 19:23:15 +0000

    feat(LimitDigit): It is the limit of digits of a float64 and dot

--- internal/data/data.go
+++ internal/data/data.go
@@ -13,10 +13,13 @@ const (
 	Float64
 )
 
+// LimitDigit is the limit of digits of a float64 and dot
+const LimitDigit uint16 = 617
+
 // It is the limit of digits of a data type
 const (
-	F64 uint16 = 309
-	F32 uint8  = 39
+	F64 uint16 = 308
+	F32 uint8  = 38
 	I64 uint8  = 18
 	I32 uint8  = 10
 	I16 uint8  = 5
