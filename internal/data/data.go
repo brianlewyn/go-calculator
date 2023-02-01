58ebcf06f1c5d94add277ce870711420cafd72ea
brianlewyn <brianlewyn@gmail.com>
2023-02-01 04:10:48 +0000

    feat: add new constants with all thethe chars of a math expression

--- internal/data/data.go
+++ internal/data/data.go
@@ -12,3 +12,35 @@ const (
 	Float32
 	Float64
 )
+
+const Numbers string = "0123456789"
+
+// Opetator
+const (
+	Mul       string = "*"
+	Add       string = "+"
+	Sub       string = "-"
+	Div       string = "/"
+	Mod       string = "%"
+	Opetators string = Mul + Add + Sub + Div + Mod
+)
+
+// Parentheses
+const (
+	Left        string = "("
+	Right       string = ")"
+	Parentheses string = Left + Right
+)
+
+// Special character
+const (
+	Dot      string = "."
+	Pow      string = "^"
+	Gap      string = " "
+	Pi       string = "π"
+	Root     string = "√"
+	Specials string = Dot + Pow + Gap + Pi + Root
+)
+
+// Runes are all characters used
+const Runes string = Numbers + Opetators + Parentheses + Specials
