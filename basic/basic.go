27a08ed15b9af2ae665a55e0b336c455b6fbde4a
brianlewyn <brianlewyn@gmail.com>
2023-02-02 05:50:32 +0000

    chore: add trim module and rename from analyse to analyser var

--- basic/basic.go
+++ basic/basic.go
@@ -1,8 +1,9 @@
 package basic
 
 import (
+	"github.com/brianlewyn/go-calculator/internal/analyse"
 	"github.com/brianlewyn/go-calculator/internal/base"
-	"github.com/brianlewyn/go-calculator/internal/correct"
+	"github.com/brianlewyn/go-calculator/internal/trim"
 )
 
 type MathExpr struct {
@@ -32,14 +33,17 @@ func (e *MathExpr) answer() float64 { return 0 }
 func (e *MathExpr) process() {}
 
 func (e *MathExpr) Calculate() (float64, error) {
-	analyse := correct.NewAnalyser(&e.Expr)
+	trim.Gaps(&e.Expr)
 
-	if analyse.IsCorrectSyntax() {
-		return 0, analyse.Error()
+	analyser := analyse.New(&e.Expr)
+
+	if analyser.IsCorrectSyntax() {
+		return 0, analyser.Error()
 	}
 
-	if analyse.IsCorrectMathExpr() {
-		return 0, analyse.Error()
+	if analyser.IsCorrectMathExpr() {
+		return 0, analyser.Error()
 	}
+
 	return e.answer(), nil
 }
