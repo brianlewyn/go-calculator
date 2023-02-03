e7040767bd084827f3286a375c6de021961a59a1
brianlewyn <brianlewyn@gmail.com>
2023-02-03 02:18:17 +0000

    feat(areCorrectNumbers): Is a method that checks if the numbers are correct

--- internal/analyse/expression.go
+++ internal/analyse/expression.go
@@ -1,6 +1,36 @@
 package analyse
 
-func (a analyse) inconsistentNumbers() {}
+import (
+	"github.com/brianlewyn/go-calculator/ierr"
+	"github.com/brianlewyn/go-calculator/internal/data"
+)
+
+func (a *analyse) areCorrectNumbers() bool {
+	digit, isDot := uint16(0), false
+
+	for i, r := range *a.expr {
+		if !(data.Numbers(&r) || r == data.Dot) {
+			isDot = false
+			digit = 0
+			continue
+		}
+
+		if r == data.Dot {
+			if isDot {
+				*a.err = ierr.Rune{R: &r, I: &i}.Dot()
+				return false
+			}
+			isDot = true
+		}
+
+		if digit++; digit == data.DigitLimit {
+			*a.err = ierr.Rune{R: &r, I: &i}.Digit()
+			return false
+		}
+	}
+
+	return true
+}
 
 func (a analyse) inconsistentOperators() {}
 
@@ -10,9 +40,12 @@ func (a analyse) inconsistentBlankSpakces() {}
 
 func (a analyse) inconsistentDecimalPoints() {}
 
-func (a *analyse) IsCorrectMathExpr() bool {
+func (a *analyse) IsCorrectExpression() bool {
 
-	a.inconsistentNumbers()
+	if !a.areCorrectNumbers() {
+		return false
+	}
+
 	a.inconsistentOperators()
 	a.inconsistentParentheses()
 	a.inconsistentBlankSpakces()
