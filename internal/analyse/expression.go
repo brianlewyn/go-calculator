9e801783654f6e9a1e065d9c5c73dd697088e3ec
brianlewyn <brianlewyn@gmail.com>
2023-02-04 00:51:40 +0000

    feat: create isGoodStart and isGoodFinal method

--- internal/analyse/expression.go
+++ internal/analyse/expression.go
@@ -5,6 +5,29 @@ import (
 	"github.com/brianlewyn/go-calculator/internal/data"
 )
 
+// isGoodStart
+func (a *analyse) isGoodStart() bool {
+	start := 0
+	char := rune((*a.expr)[start])
+	if !data.IsFirstChar(&char) {
+		*a.err = ierr.Rune{R: &char, I: &start}.Start()
+		return false
+	}
+	return true
+}
+
+// isGoodFinal
+func (a *analyse) isGoodFinal() bool {
+	end := len(*a.expr) - 1
+	char := rune((*a.expr)[end])
+	if !data.IsLastChar(&char) {
+		*a.err = ierr.Rune{R: &char, I: &end}.Final()
+		return false
+	}
+	return false
+}
+
+// areCorrectNumbers
 func (a *analyse) areCorrectNumbers() bool {
 	digit, isDot := uint16(0), false
 
@@ -32,14 +55,20 @@ func (a *analyse) areCorrectNumbers() bool {
 	return true
 }
 
+// areCorrectOperators
 func (a analyse) areCorrectOperators() bool { return true }
 
+// areCorrectParentheses
 func (a analyse) areCorrectParentheses() bool { return true }
 
+// areCorrectDots
 func (a analyse) areCorrectDots() bool { return true }
 
+// IsCorrectExpression
 func (a *analyse) IsCorrectExpression() bool {
 	switch {
+	case a.isGoodStart():
+	case a.isGoodFinal():
 	case a.areCorrectNumbers():
 	case a.areCorrectOperators():
 	case a.areCorrectParentheses():
