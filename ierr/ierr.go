52785804603ed752d935c6ca61bdb348f2f7b8e3
brianlewyn <brianlewyn@gmail.com>
2023-02-04 00:52:37 +0000

    feat: create Start and Final method and As func

--- ierr/ierr.go
+++ ierr/ierr.go
@@ -1,6 +1,9 @@
 package ierr
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type kind string
 
@@ -12,9 +15,11 @@ const (
 
 // !What error occurred?
 const (
-	IncorrectCharacter = kind("incorrect character")
+	IncorrectCharacter = kind("this character is incorrect")
 	DuplicateDot       = kind("this dot is duplicated")
 	DigitLimit         = kind("this digit exceeds the digit limit")
+	FirstChar          = kind("this character cannot be the beginning")
+	LastChar           = kind("this character cannot be the end")
 )
 
 // !Interface errors
@@ -44,6 +49,14 @@ func (r Rune) Digit() error {
 	return wrapRune(Expression, DigitLimit, r.R, &Rune{I: r.I})
 }
 
+func (r Rune) Start() error {
+	return wrapRune(Expression, FirstChar, r.R, &Rune{I: r.I})
+}
+
+func (r Rune) Final() error {
+	return wrapRune(Expression, LastChar, r.R, &Rune{I: r.I})
+}
+
 // wrap add a wrapper of type error to the already created error
 func wrap(kind kind, err error) error {
 	return fmt.Errorf("%s: %w", kind, err)
@@ -53,4 +66,12 @@ func wrap(kind kind, err error) error {
 // Add three wrappers of type error to the already created error
 func wrapRune(k1, k2 kind, r *rune, err error) error {
 	return wrap(k1, wrap(k2, fmt.Errorf("char=%q index=%w", *r, err)))
+}
+
+// As is similar to errors.As func of standard library
+func As(err error, target kind) bool {
+	if !strings.Contains(fmt.Sprint(err), string(target)) {
+		return false
+	}
+	return true
 }
