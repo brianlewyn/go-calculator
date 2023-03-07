16e7a3b6e3a59e03f24b563ddd8055aaa63b198a
brianlewyn <brianlewyn@gmail.com>
2023-03-07 06:57:20 +0000

    feat(Tokenizer): add a new test for the tokenize module func

--- internal/tokenize/tokenize_test.go
+++ internal/tokenize/tokenize_test.go
@@ -23,10 +23,7 @@ func Test_tokenize_linkedList(t *testing.T) {
 
 		gotList, err := tokenizer.linkedList()
 		assert.ErrorIsf(err, ierr.EmptyField, "ierr.EmptyField != %v", err)
-
-		wantList := d.NewDoubly[*data.Token]()
-
-		assert.Equal(gotList.Size(), wantList.Size(), "gotList.Size() != wantList()")
+		assert.Equal(gotList.Size(), 0, "gotList.Size() != 0")
 	})
 
 	t.Run("From an expression with some inappropriate symbols to a list", func(t *testing.T) {
@@ -79,22 +76,7 @@ func Test_tokenize_linkedList(t *testing.T) {
 		wantList.Append(d.NewNode(data.NewSubToken()))
 		wantList.Append(d.NewNode(data.NewNumToken("1.234")))
 
-		node1 := gotList.Head()
-		node2 := wantList.Head()
-
-		for i := 0; node1 != nil && node2 != nil; i++ {
-			token1 := node1.Data()
-			token2 := node2.Data()
-
-			if assert.EqualValues(token1.Kind(), token2.Kind(), "kind1 != kind2") {
-				if token1.Kind() == data.NumToken {
-					assert.EqualValues(*token1.Value(), *token2.Value(), "value1 != value2")
-				}
-			}
-
-			node1 = node1.Next()
-			node2 = node2.Next()
-		}
+		areEqualList(t, gotList, wantList)
 
 		assert.Equal(gotList.Size(), wantList.Size(), "gotList.Size() != wantList()")
 		assert.Equal(expression, "", "expression != \"\"")
@@ -143,39 +125,40 @@ func Test_tokenize_rebuild(t *testing.T) {
 		wantList.Append(d.NewNode(data.NewAddToken()))
 		wantList.Append(d.NewNode(data.NewNumToken("10")))
 		wantList.Append(d.NewNode(data.NewRightToken()))
-		// *
+		// *(0-12)
 		wantList.Append(d.NewNode(data.NewMulToken()))
-		// (0-12)
 		wantList.Append(d.NewNode(data.NewLeftToken()))
 		wantList.Append(d.NewNode(data.NewNumToken("0")))
 		wantList.Append(d.NewNode(data.NewSubToken()))
 		wantList.Append(d.NewNode(data.NewNumToken("12")))
 		wantList.Append(d.NewNode(data.NewRightToken()))
-		// *
+		// *(*12)
 		wantList.Append(d.NewNode(data.NewMulToken()))
-		// (*12)
 		wantList.Append(d.NewNode(data.NewLeftToken()))
 		wantList.Append(d.NewNode(data.NewMulToken()))
 		wantList.Append(d.NewNode(data.NewNumToken("12")))
 		wantList.Append(d.NewNode(data.NewRightToken()))
 
-		node1 := gotList.Head()
-		node2 := wantList.Head()
-
-		for i := 0; node1 != nil && node2 != nil; i++ {
-			token1 := node1.Data()
-			token2 := node2.Data()
-
-			if assert.EqualValues(token1.Kind(), token2.Kind(), "kind1 != kind2") {
-				if token1.Kind() == data.NumToken {
-					assert.EqualValues(*token1.Value(), *token2.Value(), "value1 != value2")
-				}
-			}
-
-			node1 = node1.Next()
-			node2 = node2.Next()
-		}
-
+		areEqualList(t, gotList, wantList)
 		assert.Equal(gotList.Size(), wantList.Size(), "gotList.Size() != wantList()")
 	})
+}
+
+func areEqualList(t *testing.T, got, want *d.Doubly[*data.Token]) {
+	node1 := got.Head()
+	node2 := want.Head()
+
+	for i := 0; node1 != nil && node2 != nil; i++ {
+		token1 := node1.Data()
+		token2 := node2.Data()
+
+		if assert.EqualValues(t, token1.Kind(), token2.Kind(), "kind1 != kind2") {
+			if token1.Kind() == data.NumToken {
+				assert.EqualValues(t, *token1.Value(), *token2.Value(), "value1 != value2")
+			}
+		}
+
+		node1 = node1.Next()
+		node2 = node2.Next()
+	}
 }
--- internal/tokenize/tokenizer_test.go
+++ internal/tokenize/tokenizer_test.go
@@ -1,35 +1,67 @@
 package tokenize
 
 import (
-	"reflect"
 	"testing"
 
+	"github.com/brianlewyn/go-calculator/ierr"
 	"github.com/brianlewyn/go-calculator/internal/data"
 	d "github.com/brianlewyn/go-linked-list/doubly"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTokenizer(t *testing.T) {
-	type args struct {
-		data *data.Data
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *d.Doubly[*data.Token]
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Tokenizer(tt.args.data)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Tokenizer() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Tokenizer() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	assert := assert.New(t)
+
+	t.Run("From an empty expression to a linked list ", func(t *testing.T) {
+		expression := ""
+		gotList, err := Tokenizer(data.New(&expression))
+		assert.ErrorIsf(err, ierr.EmptyField, "ierr.EmptyField != %v", err)
+		assert.Nil(gotList, "gotList != nil")
+	})
+
+	t.Run("From an expression to a empty linked list ", func(t *testing.T) {
+		expression := "  "
+		gotList, err := Tokenizer(data.New(&expression))
+		assert.ErrorIsf(err, ierr.EmptyField, "ierr.EmptyField != %v", err)
+		assert.Nil(gotList, "gotList != nil")
+	})
+
+	t.Run("From an expression to a linked list", func(t *testing.T) {
+		expression := "(0 - 1 + 2 * 3 / 4 ^ 5 % 6 + √π)(-1.234)"
+
+		gotList, err := Tokenizer(data.New(&expression))
+		assert.NoError(err, "Tokenizer(data) error != nil")
+
+		wantList := d.NewDoubly[*data.Token]()
+
+		// (0-1+2*3/4^5%6+√π)
+		wantList.Append(d.NewNode(data.NewLeftToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("0")))
+		wantList.Append(d.NewNode(data.NewSubToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("1")))
+		wantList.Append(d.NewNode(data.NewAddToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("2")))
+		wantList.Append(d.NewNode(data.NewMulToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("3")))
+		wantList.Append(d.NewNode(data.NewDivToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("4")))
+		wantList.Append(d.NewNode(data.NewPowToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("5")))
+		wantList.Append(d.NewNode(data.NewModToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("6")))
+		wantList.Append(d.NewNode(data.NewAddToken()))
+		wantList.Append(d.NewNode(data.NewRootToken()))
+		wantList.Append(d.NewNode(data.NewPiToken()))
+		wantList.Append(d.NewNode(data.NewRightToken()))
+
+		// *(-1.234)
+		wantList.Append(d.NewNode(data.NewMulToken()))
+		wantList.Append(d.NewNode(data.NewLeftToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("0")))
+		wantList.Append(d.NewNode(data.NewSubToken()))
+		wantList.Append(d.NewNode(data.NewNumToken("1.234")))
+		wantList.Append(d.NewNode(data.NewRightToken()))
+
+		areEqualList(t, gotList, wantList)
+	})
 }
